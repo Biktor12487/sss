4fa1e7914f0433d0194fb7e5aedd21a1219242fb
Your Name <biktor12487@gmail.com>
2017-08-13 19:54:58 +0300

    oo

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $(".owl-recall").owlCarousel({
+  	items: 2,
+  	nav: true,
+  	navText: false
+  });
+  $(".owl-date1").owlCarousel({
+  	items: 3,
+  	nav: true,
+  	navText: false
+  });
+
+$(".owl-date2").owlCarousel({
+  	items: 3,
+  	nav: true,
+  	navText: false
+  });
+
+});
