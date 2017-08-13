9dbfbcba02bcb4226db3526dfb356339b633875a
Your Name <biktor12487@gmail.com>
2017-08-13 22:56:57 +0300

    ssssz

--- js/main.js
+++ js/main.js
@@ -2,18 +2,54 @@ $(document).ready(function(){
   $(".owl-recall").owlCarousel({
   	items: 2,
   	nav: true,
-  	navText: false
+  	navText: false,
+    responsive: {
+                0: {
+                    items: 1,
+                },
+                768: {
+                    items: 2,
+                },
+                1024: {
+                    items: 2,
+                }
+            }
   });
   $(".owl-date1").owlCarousel({
   	items: 3,
   	nav: true,
-  	navText: false
+  	navText: false,
+        responsive: {
+                0: {
+                    items: 1,
+                },
+                768: {
+                    items: 2,
+                },
+                1024: {
+                    items: 2,
+                }
+            }
   });
 
 $(".owl-date2").owlCarousel({
   	items: 3,
   	nav: true,
-  	navText: false
+  	navText: false,
+        responsive: {
+                0: {
+                    items: 1,
+                },
+                768: {
+                    items: 2,
+
+                },
+                1024: {
+                    items: 2,
+                    nav: true,
+
+                }
+            }
   });
 
 });
